5413cc8711d7ab4b1e73904ed15c61e674ac4313
kundan-gts <kundan.panwar@gtsinfosoft.com>
2025-01-22 11:29:55 +0530

    project upload

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main(){
+
+   runApp(MaterialApp(
+      home: animation()
+   ));
+
+}
+
+class animation extends StatefulWidget {
+  animation({super.key});
+
+  @override
+  State<animation> createState() => _animationState();
+}
+
+class _animationState extends State<animation> {
+  double w=200;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+
+          children: [
+            // container
+            AnimatedContainer(
+              height: 200,
+              width: w,
+              decoration: BoxDecoration(
+                  image: DecorationImage(image: AssetImage("./assets/car.png"),fit: BoxFit.fill)
+              ), duration: Duration(seconds: 1),
+            ),
+            ElevatedButton(onPressed: (){
+                setState(() {
+                   w==200 ? w=400: w=200;
+                });
+            }, child: Text("move")),
+          ],
+        ),
+      ),
+    );
+  }
+}
