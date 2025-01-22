ee857f713520807b9c5aa68535128754ba022bd1
kundan-gts <kundan.panwar@gtsinfosoft.com>
2025-01-22 16:28:55 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+
 import 'package:flutter/material.dart';
 
 void main(){
 
    runApp(MaterialApp(
+     debugShowCheckedModeBanner: false,
       home: animation()
    ));
 
@@ -16,29 +18,59 @@ class animation extends StatefulWidget {
 }
 
 class _animationState extends State<animation> {
-  double w=200;
+
+  Matrix4 m = Matrix4.rotationY(0);
+  Alignment alig= Alignment.centerLeft;
+  Alignment bg= Alignment.centerLeft;
+  Alignment en=Alignment.centerRight;
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      body: Center(
+      body: Container(
+        width: double.infinity,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
 
           children: [
             // container
-            AnimatedContainer(
-              height: 200,
-              width: w,
-              decoration: BoxDecoration(
-                  image: DecorationImage(image: AssetImage("./assets/car.png"),fit: BoxFit.fill)
-              ), duration: Duration(seconds: 1),
+            TweenAnimationBuilder(
+              tween: Tween(begin: bg,end:en),
+              duration: Duration(seconds: 2),
+              curve: Curves.bounceOut,
+              builder:(context,value,widget) {
+                return Align(
+                  alignment: value,
+                  child: Transform(
+                    alignment: Alignment.center,
+                    transform: m,
+                    child: Container(
+                        height: 200,
+                        width: 250,
+                        decoration: const  BoxDecoration(
+                            image: DecorationImage(image: AssetImage(
+                                "./assets/car.png"), fit: BoxFit.fill,)
+                        ),
+
+                      ),
+                    ),
+                  );
+                },
+                onEnd: (){
+                    setState(() {
+                         var tmp=bg;
+                         bg=en;
+                         en=tmp;
+                         m==Matrix4.rotationY(0) ? m=Matrix4.rotationY(3.14159): m=Matrix4.rotationY(0);
+                    });
+                },
             ),
             ElevatedButton(onPressed: (){
                 setState(() {
-                   w==200 ? w=300: w=200;
+                  alig==Alignment.centerLeft ? alig=Alignment.centerRight: alig=Alignment.centerLeft;;
                 });
             }, child: Text("move")),
+
           ],
         ),
       ),
